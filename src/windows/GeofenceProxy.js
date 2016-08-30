ebfb19df0743d8fdb6bd6609a93cb5b4deb08aa1
Antonino Tocco <antonino.tocco90@gmail.com>
2016-08-30 20:50:37 +0200

    add new newtonsoft.json.dll

--- src/windows/GeofenceProxy.js
+++ src/windows/GeofenceProxy.js
@@ -16,7 +16,6 @@
             geoN.notificationTitle = geo.notification.title;
             geoN.openAppOnClick = geo.notification.openAppOnClick;
             geoN.data = JSON.stringify(geo.notification.data);
-            console.log(GeofenceComponent.GeoNotificationManager);
             GeofenceComponent.GeoNotificationManager.addOrUpdate(geoN);
         });
         success && success();
